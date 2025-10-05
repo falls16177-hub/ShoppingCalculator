27dc45eacf8ef92c7076ad3526b7c4a8cea3c87f
ace <falls.16177@gmail.com>
2025-10-05 14:38:15 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+import  java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter price of the item: ");
+        int ItemPrice = input.nextInt();
+        double ShipCost = ItemPrice * 0.02;
+        double TotalCost = ItemPrice + ShipCost;
+
+        if (ItemPrice >= 100) {
+            System.out.print("Shipping is free your total price is: " + ItemPrice);
+        } else if (ItemPrice < 100) {
+            System.out.print("Your total price is: " + TotalCost);
+
+        }
+    }
 }
